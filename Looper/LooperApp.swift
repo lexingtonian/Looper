35be9ad85de8774083bf32c2e8ef337f611225fd
lexingtonian <ari@linux.com>
2024-09-16 18:24:24 +0300

    Initial Commit

---
+++ Looper/LooperApp.swift
@@ -0,0 +1,17 @@
+//
+//  LooperApp.swift
+//  Looper
+//
+//  Created by Ari Jaaksi on 16.9.2024.
+//
+
+import SwiftUI
+
+@main
+struct LooperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
